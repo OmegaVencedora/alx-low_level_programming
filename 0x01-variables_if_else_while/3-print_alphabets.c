0d7d33450817fcc4551e4891c2c3c08978aacb17
OmegaVencedora <andeerivers51@gmail.com>
2022-10-15 08:24:11 -0700

    update

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print uppercase letters
+ * Return: always 0
+ */
+int main(void)
+{
+	char Lc;
+	char lc;
+	for (lc = 'a'; lc <= 'z'; lc++);
+	{
+		putchar(lc)
+	}
+	for (Lc = 'A'; Lc <= 'Z'; lc++);
+	{
+		putchar(Lc);
+	}
+	putchar('\n');
+	Return(0)
+}
