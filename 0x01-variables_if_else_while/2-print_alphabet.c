528fb90e4be53f58783b69cddfecad8ae5b30f3c
OmegaVencedora <andeerivers51@gmail.com>
2022-10-15 12:38:32 -0700

    update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- * main - prints alphabet in lower case 
+ * main - prints alphabet in lower case
  * Return: always 0
  */
 int main(void)
 {
 	char lc;
 
-	for (lc = 'a'; lc <= 'z'; lc++);
+	for (lc = 'a'; lc <= 'z'; lc++)
 	{
 		putchar(lc);
 	}
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
