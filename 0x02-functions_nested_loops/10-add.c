d89635fbc100491334e5bff4a8ae7d45c84d3831
OmegaVencedora <andeerivers51@gmail.com>
2022-10-18 23:41:51 -0700

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main"
+
+/**
+ * add - add two numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the addition of both numbere
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
