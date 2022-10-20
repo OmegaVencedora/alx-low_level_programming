4986d2b88165876297b553ed46c681a87e684b8d
OmegaVencedora <andeerivers51@gmail.com>
2022-10-20 12:36:25 -0700

    update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0-9
+ * Return: 0 always
+ */
+
+void print_numbers(void)
+{
+	for (a = '0' ; a <= 9 ; a++)
+		_putchar(a);
+	_putchar('\n');
+	return (0);
+}
